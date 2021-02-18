4ae5581eec0883f4c39b3528c9aa61f4c77277bb
Juan Pablo Chica <juanpchica@pdc.magnetron.com>
2021-02-18 13:09:09 -0500

    Init files added

---
+++ src/main/java/com/api/Moldes/MoldesApplication.java
@@ -0,0 +1,13 @@
+package com.api.Moldes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoldesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoldesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Moldes/MoldesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Moldes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoldesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
